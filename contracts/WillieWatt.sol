07aa675a3763708e55ebb6426b42a515b068944c
dsummers91 <deonsummers91@gmail.com>
2017-01-25 00:02:35 -0600

    initial commit

---
+++ contracts/WillieWatt.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.4.6;
+
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+
+contract WillieWatt {
+
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function WillieWatt(
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {              
+        totalSupply = 0;                        
+        name = tokenName;   
+        symbol = tokenSymbol;   
+        decimals = decimalUnits;  
+    }
+
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;          
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; 
+        balanceOf[msg.sender] -= _value;                 
+        balanceOf[_to] += _value;                    
+        Transfer(msg.sender, _to, _value);             
+    }
+
+
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }        
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function refund(address _recipient, uint256 _value) return (bool success) {
+      if(!msg.sender.send(msg.value / 1 ether / 1000)) throw;
+      balanceOf[msg.sender] -= msg.value / 1 ether / 1000;
+      totalSupply -= msg.value / 1 ether / 1000;
+      Transfer(this, msg.sender, msg.value / 1 ether / 1000);
+      return true
+    }
+
+    function() payable {
+      balanceOf[msg.sender] += msg.value * 1 ether * 1000;
+      totalSupply += msg.value * 1 ether * 1000;
+      Transfer(msg.sender, this, msg.value * 1 ether * 1000);
+      return true
+    }
+}
