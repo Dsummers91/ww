0d915904d2c3c08eed3a7f2dd56571fedc330f7e
Deon <deonsummers91@gmail.com>
2017-01-25 12:22:39 -0600

    i suck at coding

--- contracts/WillieWatt.sol
+++ contracts/WillieWatt.sol
@@ -67,17 +67,17 @@ contract WillieWatt {
     }
 
     function refund(address _recipient, uint256 _value) returns (bool success) {
-      balanceOf[msg.sender] -= msg.value / 1 ether / 1000;
-      totalSupply -= msg.value / 1 ether / 1000;
+      balanceOf[msg.sender] -= _value;
+      totalSupply -= _value;
       if(balanceOf[msg.sender] < 0) throw;                      // Do not process is balance will fall below 0;
-      if(!msg.sender.send(msg.value / 1 ether / 1000)) throw;
-      Transfer(this, msg.sender, msg.value / 1 ether / 1000);
+      if(!msg.sender.send(_value / 1000)) throw;
+      Transfer(this, msg.sender, _value);
       return true;
     }
 
     function() payable {
-      balanceOf[msg.sender] += msg.value * 1 ether * 1000;
-      totalSupply += msg.value * 1 ether * 1000;
-      Transfer(msg.sender, this, msg.value * 1 ether * 1000);
+      balanceOf[msg.sender] += msg.value * 1000;
+      totalSupply += msg.value * 1000;
+      Transfer(msg.sender, this, msg.value * 1000);
     }
 }
